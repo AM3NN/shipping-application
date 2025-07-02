5a65dc579a2b8f7cb03ca5a5ab23f39b772abbf2
AM3NN <amenallah.laouini@esprit.tn>
2025-07-02 15:04:33 +0100

    Initial commit: Scaffolded microservices architecture

---
+++ backend/ai-prediction-service/src/test/java/tn/epac/aipredictionservice/AiPredictionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.epac.aipredictionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPredictionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

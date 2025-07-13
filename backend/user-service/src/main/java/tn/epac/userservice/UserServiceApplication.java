42b7fdfbbf1d33535d2732fe3c4197e292ea8c32
rahmabensaidd <bensaidrahma2024@gmail.com>
2025-07-13 11:53:51 +0100

    changes

--- backend/user-service/src/main/java/tn/epac/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/tn/epac/userservice/UserServiceApplication.java
@@ -2,10 +2,11 @@ package tn.epac.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+@EnableDiscoveryClient
 @EnableMongoRepositories(basePackages = "tn.epac.userservice.Repository")
 @SpringBootApplication
 @EnableScheduling

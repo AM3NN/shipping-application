5f0361a70480db864dfbfcdb5290c7f2048e2d18
AM3NN <amenallah.laouini@esprit.tn>
2025-09-25 10:34:09 +0100

    testing webhook v3

--- backend/billing-service/src/main/java/tn/epac/billingservice/service/InvoiceService.java
+++ backend/billing-service/src/main/java/tn/epac/billingservice/service/InvoiceService.java
@@ -61,7 +61,7 @@ public class InvoiceService {
         sendInvoiceEmail(saved, pdfBytes, customerName, authToken);
         return saved;
     }
-//ffffff
+//fffffff
     private String extractCustomerNameFromJwt(String authToken) {
         try {
             Jwt jwt = jwtDecoder.decode(authToken);
